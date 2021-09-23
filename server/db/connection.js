46e1f096e0c8bcd1d1f2ca29d0d97b41768c7077
Jon Jackson <ocskier@gmail.com>
2021-09-23 12:24:55 -0400

    feat: adds connection via mongoose

---
+++ server/db/connection.js
@@ -0,0 +1,36 @@
+import chalk from 'chalk';
+import mongoose from 'mongoose';
+
+mongoose.connect(
+  process.env.MONGODB_URL || 'mongodb://localhost:27017/cloudinary-storage',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set('debug', (collectionName, methodName, ...methodArgs) =>
+  console.info(
+    chalk.bold.black.bgWhite(
+      `Mongoose: ${collectionName} - ${methodName}(${JSON.stringify(
+        methodArgs
+      )})`
+    )
+  )
+);
+
+const db = mongoose.connection;
+
+export function connectToDB(cb) {
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () {
+    console.log(
+      chalk.bold.green(
+        `Successful connection to ${
+          process.env.MONGODB_URL || 'mongodb://localhost:27017/chat'
+        }`
+      )
+    );
+    cb();
+  });
+}
