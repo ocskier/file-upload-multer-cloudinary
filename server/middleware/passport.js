ca328f38577ccaae57006b375450fcee8bf2651c
Jon Jackson <ocskier@gmail.com>
2021-09-23 13:02:55 -0400

    feat: add basic passport local strategy

---
+++ server/middleware/passport.js
@@ -0,0 +1,35 @@
+import passport from 'passport';
+import { LocalStrategy } from 'passport-local';
+
+// import db from '../models/index.js';
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser(async (_id, done) => {
+  console.log(_id);
+  // const matchingUser = await db.User.findById(_id).populate('messages');
+  done(null, _id);
+});
+
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    try {
+      // const user = await User.findOne({ username: username });
+      // if (!user) {
+      //   return done(null, false);
+      // }
+      // if (!user.verifyPassword(password)) {
+      //   return done(null, false);
+      // }
+      // test user
+      const user = { id: 1, username: 'newuser', password: 'newpassword' };
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+export default passport;
