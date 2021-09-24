ed503867314f948056b21407952b25d489702dee
Jon Jackson <ocskier@gmail.com>
2021-09-24 01:17:40 -0400

    feat: adds drag and drop, preview working FS file upload

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,34 +2,69 @@ const photoFormSubmit = document.querySelector(
   '.photo-form .ui.submit.button'
 );
 const photoFileInput = document.querySelector('#file-upload');
+let preview = document.querySelector('.ui.card .image');
 
 const fileReader = new FileReader();
 fileReader.onload = function (e) {
-    document
-      .querySelector('.ui.card .image img')
-      .setAttribute('src', e.target.result);
+    preview.childNodes[1].setAttribute('src', e.target.result);
 };
 
-const handleFileChange = async (e) => {
+const handleFileChange = (e) => {
     fileReader.readAsDataURL(photoFileInput.files[0]);
 }
 
-const handleFileSubmit = async (e) => {
+const handleFileSubmit = (e) => {
     e.preventDefault(); // not really needed here but good form 
-    console.log(photoFileInput.files[0]);
+    handleFileUpload(photoFileInput.files);
+}
+
+const handleFileUpload = async (files) => {
+    console.log(files[0]);
     const multiFormData = new FormData();
     multiFormData.append('image-file', photoFileInput.files[0]);
     try {
-        const response = await fetch('/api/upload',{
-            method: 'POST',
-            body: multiFormData,
-        });
-        const data = await response.json();
-        console.log(data);
-    } catch(error) {
-        console.log(error);
+      const response = await fetch('/api/upload', {
+        method: 'POST',
+        body: multiFormData,
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
     }
 }
 
+const drop = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+
+  const dt = e.dataTransfer;
+  const files = dt.files;
+  photoFileInput.files = files;
+  preview.style.color = 'transparent';
+  fileReader.readAsDataURL(files[0]);
+}
+
+const dragenter = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+const dragleave = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+  preview.style.backgroundColor = 'transparent';
+}
+
+const dragover = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+  preview.style.backgroundColor = 'grey';
+}
+
+preview.addEventListener('dragenter', dragenter, false);
+preview.addEventListener('dragleave', dragleave, false);
+preview.addEventListener('dragover', dragover, false);
+preview.addEventListener('drop', drop, false);
 photoFileInput.addEventListener('change', handleFileChange);
 photoFormSubmit.addEventListener('click', handleFileSubmit);
